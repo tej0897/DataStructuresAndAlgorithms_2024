9a4bd086dede44eddee46d8b21cb60acc6a45aa9
tej0897 <tej897@gmail.com>
2024-04-15 19:42:28 +0530

    bubble sort update

--- src/main/java/DataStructures/SortingAlgo/BubbleSort.java
+++ src/main/java/DataStructures/SortingAlgo/BubbleSort.java
@@ -1,4 +1,39 @@
 package DataStructures.SortingAlgo;
 
 public class BubbleSort {
+    public static void main(String[] args) {
+
+        int [] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastUnsortedIndex = intArray.length-1; lastUnsortedIndex > 0; lastUnsortedIndex --){
+            for (int i = 0; i < lastUnsortedIndex; i++){
+                if (intArray[i] > intArray[i+1]){
+                    swapNumbers(intArray, i, i+1);
+                }
+            }
+        }
+
+        for (int j : intArray){
+            System.out.println(j);
+        }
+
+    }
+
+    // swap method
+
+    public static void swapNumbers(int[] arr, int a, int b){
+
+        if (a == b){
+            return;
+        }
+
+        int temp;
+
+        // swapping code
+
+        temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
 }
